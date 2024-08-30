90b51c5e3748fc7a2465ec6d37c3fa34f67009e1
komal <komalkhatria29@gmail.com>
2024-08-30 23:16:49 +0530

    Swapped first and last element of array using swap  method

--- Array/ElementSwap.java
+++ Array/ElementSwap.java
@@ -1,5 +1,14 @@
 import java.util.Scanner;
 public class ElementSwap {
+    //METHOD TO SWAP FIRST AND LAST ELEMENT OF ARRAY
+    static void swapElements(int a[]){
+        int temp= a[0];
+        a[0]= a[a.length-1];
+        a[a.length-1]= temp;
+        for(int i=0; i<a.length; i++){
+            System.out.print(a[i] + " ");
+        }
+    }
     public static void main(String args[]){
         Scanner sc= new Scanner(System.in);
 
@@ -12,12 +21,17 @@ public class ElementSwap {
             arr[i]= sc.nextInt();
         }
 
-        //FIRST AND LAST ELEMENT SWAP WITHOUT USING METHOD
-        int temp= arr[0];
-        arr[0]= arr[size-1];
-        arr[size-1]= temp;
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i] + " ");
-        }
+//        //FIRST AND LAST ELEMENT SWAP WITHOUT USING METHOD
+//        int temp= arr[0];
+//        arr[0]= arr[size-1];
+//        arr[size-1]= temp;
+//        for(int i=0; i<arr.length; i++){
+//            System.out.print(arr[i] + " ");
+//        }
+
+        //CALLING SWAP ELEMENT METHOD
+        swapElements(arr);
+
+
     }
 }
