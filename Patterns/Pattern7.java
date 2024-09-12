009141ea00fe8112c12d346e18a77f2ed4848b92
komal <komalkhatria29@gmail.com>
2024-09-12 22:28:34 +0530

    square pattern with row numbers

---
+++ Patterns/Pattern7.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Pattern7{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Enter a size: ");
+        int size= sc.nextInt();
+        for(int i=1; i<=size; i++){
+            for(int j=1; j<=size; j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
