9fa3d21c15bd16e2890444b5cebae047b81b450e
komal <komalkhatria29@gmail.com>
2024-09-16 12:23:00 +0530

    find, count and print positions of duplicate elements of array

--- Array/DuplicateElement.java
+++ Array/DuplicateElement.java
@@ -1,20 +1,32 @@
 import java.util.Scanner;
 public class DuplicateElement {
+    static void findDuplicateElements(int arr[]){
+        boolean visited[]= new boolean[arr.length];
 
-    //COUNT NO. OF DUPLICATES OF TARGET ELEMENT
-    static void findDuplicateElements(int arr[], int target){
-        int count=0;
         for(int i=0; i<arr.length; i++){
-            if(arr[i]==target){
-                for(int j=i+1; j<arr.length; j++){
-                        if(arr[j]==target){
-                            count++;
-                        }
-                }
-                break;
+            //SKIP IF ELEMENT IS ALREADY CHECKED
+            if(visited[i]){
+                continue;
             }
+
+            int count=0, first_index=0, last_index=0;
+            for(int j=i+1; j<arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    visited[j]=true;    //MARK THIS ELEMENT AS CHECKED
+                    count++;
+                    last_index=j;
+                    System.out.print(j+ " ");
+                }
+            }
+
+            if(count>0){
+                System.out.println("(Index position), " + arr[i] + " duplicates " + count + " times");
+                System.out.println("First Index: " + i);
+                System.out.println("Last Index: " + last_index);
+                System.out.println();
+            }
+
         }
-        System.out.println("Number of duplicates: " + count);
     }
 
     public static void main(String[] args) {
@@ -28,8 +40,8 @@ public class DuplicateElement {
         for(int i=0; i<arr.length; i++){
             arr[i]=sc.nextInt();
         }
-        System.out.print("Enter a number to check its duplicates: ");
-        int target= sc.nextInt();
-        findDuplicateElements(arr, target);
+
+        findDuplicateElements(arr);
+
     }
 }
