20646fb5fea670eedbdbe7f3f231f6523fbbad89
komal <komalkhatria29@gmail.com>
2024-09-17 11:31:37 +0530

    half diamond shape pattern

---
+++ Patterns/Pattern15.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Pattern15 {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Enter a size: ");
+        int size= sc.nextInt();
+
+        for(int i=1; i<=size; i++){
+            for(int s=size-1; s>=i; s--){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=size-1; i>=1; i--){
+            for(int s=size-1; s>=i; s--){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
