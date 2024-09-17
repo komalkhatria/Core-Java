e8f448dac60bb906eea98cebaa55508b6c923b6e
komal <komalkhatria29@gmail.com>
2024-09-17 11:34:31 +0530

    triangle pattern

---
+++ Patterns/Pattern16.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Pattern16 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a size: ");
+        int size= sc.nextInt();
+
+        for(int i=1; i<=size; i++){
+            for(int s=size-1; s>=i; s--){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+
+    }
+}
