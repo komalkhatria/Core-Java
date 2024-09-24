7ed26af9f497bac286600ed17d6876a4b1977337
komal <komalkhatria29@gmail.com>
2024-09-24 13:31:12 +0530

    sum of elements of each row

---
+++ 2D-Array/RowWiseSum.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class RowWiseSum {
+    //METHOD TO CALCULATE SUM OF ELEMENTS OF EACH ROW OF 2D ARRAY
+    static void sumOfElements(int a[][]){
+
+        for(int i=0; i<a.length; i++){
+            int sum=0;
+            for(int j=0; j<a[i].length; j++){
+                sum+= a[i][j];      //ADDING EACH ELEMENT OF THE ROW TO SUM
+            }
+            System.out.println("Sum of row " + (i+1) + " = " + sum);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        //INPUT NO. OF ROWS AND COLUMNS
+        System.out.print("Enter no. of rows: ");
+        int rows= sc.nextInt();
+        System.out.print("Enter no. of columns: ");
+        int cols= sc.nextInt();
+
+        //DECLARING ARRAY WITH USER INPUT SIZE FOR ROWS AND COLUMNS
+        int a[][]= new int[rows][cols];
+
+        //TAKING USER INPUT ELEMENTS
+        for(int i=0; i<rows; i++){
+            System.out.print("Enter " + cols + " elements for row " + (i+1) + ": ");
+            for(int j=0; j<cols; j++){
+                a[i][j]= sc.nextInt();      //ELEMENT INPUT ON THE INTERSECTION OF rows AND cols
+            }
+        }
+
+        sumOfElements(a);
+    }
+}
