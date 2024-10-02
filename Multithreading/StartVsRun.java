593606596b2ca7f365c48d23cb026534bcac3732
komal <komalkhatria29@gmail.com>
2024-10-02 22:07:11 +0530

    difference between start and run for calling the run method

--- Multithreading/StartVsRun.java
+++ Multithreading/StartVsRun.java
@@ -11,5 +11,8 @@ public class StartVsRun {
         MyThread1 t= new MyThread1();
         t.run();        //THIS WILL CALL THE run METHOD AS A NORMAL METHOD, THE THREAD IS STILL IN INACTIVE STATE
         t.start();      //THIS WILL BRING THE THREAD (run) TO ACTIVE STATE, AND WILL EXECUTE THE THREAD
+
+        //MULTIPLE start METHOD CANNOT BE CALLED
+        t.start();      //IllegalThreadStateException
     }
 }
