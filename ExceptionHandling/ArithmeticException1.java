cde2730de3983921227783538616c20f691902f3
komal <komalkhatria29@gmail.com>
2024-10-07 23:04:31 +0530

    handling arithmetic exception using try-catch-finally

--- ExceptionHandling/ArithmeticException1.java
+++ ExceptionHandling/ArithmeticException1.java
@@ -7,10 +7,10 @@ public class ArithmeticException1 {
             System.out.println(c);
         }
 
-        /*//catch BLOCK IS USED TO HANDLE THE EXCEPTION, EXECUTES ONLY IF EXCEPTION OCCURS
+        //catch BLOCK IS USED TO HANDLE THE EXCEPTION, EXECUTES ONLY IF EXCEPTION OCCURS
         catch(ArithmeticException ex){
             System.out.println("Exception handled");
-        }*/
+        }
 
         //finally BLOCK IS USED TO HANDLE THE EXCEPTION, BUT IT RUNS EVEN IF EXCEPTION DOES NOT OCCUR
         finally{
