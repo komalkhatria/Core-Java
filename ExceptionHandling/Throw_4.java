504212a22b6435f6cad3dca64dd04691016fbf1e
komal <komalkhatria29@gmail.com>
2024-10-13 18:36:12 +0530

    attempt to throw user-defined exception, case 4

---
+++ ExceptionHandling/Throw_4.java
@@ -0,0 +1,7 @@
+public class Throw_4 {
+    public static void main(String[] args) {
+
+        //TO THROW AN EXCEPTION, THE INSTANCE MUST BE OF TYPE THROWABLE OR SUBCLASS OF THROWABLE
+        throw new Throw_4();        //CANNOT THROW ANY EXCEPTION
+    }
+}
