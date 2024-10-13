a5237a2136787f57ea5d21195425351af0d1e969
komal <komalkhatria29@gmail.com>
2024-10-13 16:42:54 +0530

    throw arithmetic exception, case 1

---
+++ ExceptionHandling/Throw_1.java
@@ -0,0 +1,8 @@
+public class Throw_1 {
+    public static void main(String[] args) {
+
+        //throw KEYWORD IS USED TO THROW AN EXCEPTION EXPLICITLY
+        //WE SPECIFY THE EXCEPTION OBJECT TO BE THROWN
+        throw new ArithmeticException("An exception");      //THE EXCEPTION HAS SOME MESSAGE WITH IT WHICH PROVIDES ERROR DSCRIPTION
+    }
+}
