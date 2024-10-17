bfc7f12659448190fd1e311753f1447f6e8e42f8
komal <komalkhatria29@gmail.com>
2024-10-17 21:50:19 +0530

    one method throws multiple exceptions

--- ExceptionHandling/Throws.java
+++ ExceptionHandling/Throws.java
@@ -1,10 +1,11 @@
 // throws keyword is used to declare an exception
 // It gives information to the programmer that there may occur an exception
 // throws is always used with method
+// Two or more exceptions can be declared together in a method using throws keyword
 
 public class Throws {
-    //DECLARE show() METHOD THAT CAN THROW AN ArithmeticException
-    static void show() throws ArithmeticException{
+    //DECLARE show() METHOD THAT CAN THROW ArithmeticException OR NullPointerException
+    static void show() throws ArithmeticException, NullPointerException{
         System.out.println("Show");
     }
     public static void main(String[] args) {
