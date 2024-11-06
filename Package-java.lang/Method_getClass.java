095087555c92641322888428604d1341a4fe1e23
komal <komalkhatria29@gmail.com>
2024-11-06 13:00:11 +0530

    object class method getClass() returns runtime class of an object

---
+++ Package-java.lang/Method_getClass.java
@@ -0,0 +1,7 @@
+//It returns the class object of “this” object and is used to get the actual runtime class of the object.
+public class Method_getClass {
+    public static void main(String[] args) {
+        Object ob= new Object();
+        System.out.println(ob.getClass());      //class java.lang.Object
+    }
+}
