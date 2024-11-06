5754474d141bb773e96d2e48b4e96a038c4aafef
komal <komalkhatria29@gmail.com>
2024-11-06 12:40:30 +0530

    object class method equals() compares content/state of two objects

---
+++ Package-java.lang/Method_equals.java
@@ -0,0 +1,9 @@
+//The equals() method compares two strings, and returns true if the strings are equal, and false if not.
+//equals method compares the content or state of two objects.
+public class Method_equals {
+    public static void main(String[] args) {
+        String s= new String("Programming");
+        String s1= "Programming";
+        System.out.println(s.equals(s1));
+    }
+}
