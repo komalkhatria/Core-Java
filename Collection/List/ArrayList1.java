4c04892bc89c0b267e7aa549db327f72e3e421e5
komal <komalkhatria29@gmail.com>
2024-11-17 23:11:34 +0530

    create generic arraylist and add objects in it

---
+++ Collection/List/ArrayList1.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class ArrayList1 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        ArrayList al= new ArrayList();      //creating generic arraylist
+        al.add("Hello");        //adding object in arraylist
+        al.add('M');
+        al.add(2024);
+        al.add(8.5);
+        System.out.println(al);
+
+        System.out.print("Enter size of arraylist: ");
+        int size= sc.nextInt();
+        ArrayList al1= new ArrayList();      //creating generic arraylist
+
+        System.out.print("Enter elements in arraylist: ");
+        for(int i=0; i<size; i++){
+            al1.add(sc.next());
+        }
+        System.out.println(al1);
+    }
+}
